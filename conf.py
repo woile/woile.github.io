e5eed460ff5498de6d627fd3f29901fe5318f31d
Santiago Fraire Willemoës <santiwilly@gmail.com>
2021-02-07 18:00:53 +0100

    feat: add new commentbox system

--- conf.py
+++ conf.py
@@ -993,12 +993,12 @@ RSS_COPYRIGHT_FORMATS = CONTENT_FOOTER_FORMATS
 # systems.  The following comment systems are supported by Nikola:
 #   disqus, facebook, intensedebate, isso, muut, commento
 # You can leave this option blank to disable comments.
-COMMENT_SYSTEM = "disqus"
+COMMENT_SYSTEM = "commentbox"
 # And you also need to add your COMMENT_SYSTEM_ID which
 # depends on what comment system you use. The default is
 # "nikolademo" which is a test account for Disqus. More information
 # is in the manual.
-COMMENT_SYSTEM_ID = "woile"
+COMMENT_SYSTEM_ID = "5692121306628096-proj"
 
 # Create index.html for page folders?
 # WARNING: if a page would conflict with the index file (usually
