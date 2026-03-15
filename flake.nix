05522fc974fd901e14b2891de077f1fe8a4a31a3
Santiago Fraire Willemoes <santiwilly@gmail.com>
2026-03-15 18:14:04 +0100

    feat: add flake

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "A development shell";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    sukr.url = "github:woile/sukr";
+    sukr.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  outputs =
+    inputs@{
+      flake-parts,
+      ...
+    }:
+    # https://flake.parts/
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        { pkgs, inputs', ... }:
+        let
+          sukr = inputs'.sukr.packages.sukr;
+        in
+        {
+          # Default shell opened with `nix develop`
+          devShells.default = pkgs.mkShell {
+            name = "dev";
+
+            # Available packages on https://search.nixos.org/packages
+            buildInputs = with pkgs; [
+              just
+              sukr
+              miniserve
+              tailwindcss
+              gemini-cli
+            ];
+
+            shellHook = ''
+              echo "Welcome to the devshell!"
+            '';
+          };
+        };
+    };
+}
